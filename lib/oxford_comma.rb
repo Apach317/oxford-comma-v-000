6fbb198052dd59d477e22df62a0dab43f49417d0
Ron Lynch <lynch.ronald1@gmail.com>
2018-09-02 03:34:21 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,13 +5,3 @@ def oxford_comma(array)
       return array.join << "and"
     end
 end
-
-if array.length == 1
-   array[0]
-
- elsif array.length == 2
-   array.join( " and ")
-
- elsif array.length >= 3
-   array[-1].insert(0, "and ")
-   new_array = array.join(", ")
