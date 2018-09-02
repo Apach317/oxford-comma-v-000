351211727c4d5a10d97e65a907c52c90b5789838
Ron Lynch <lynch.ronald1@gmail.com>
2018-09-02 03:55:21 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,10 @@
 def oxford_comma(array)
-  if array.length == 1
-  array[0]
-
-elsif array.length == 2
-  array.join( " and ")
-
-elsif array.length >= 3
-  array[-1].insert(0, "and ")
-  new_array = array.join(", ")
+    if array.length == 1
+      return array[0]
+    elsif array.length == 2
+      return array.join(" and ")
+    elsif array.length >= 3
+      return array[-1].insert(0, "and ")
+    new_array = array.join(", ")
     end
 end
